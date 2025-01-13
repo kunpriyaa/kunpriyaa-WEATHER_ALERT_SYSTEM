e754a84c3a8c6f2704cdf79bffd263299aac9809
kunpriyaa <kunpriyachaiwong21@!gmail.com>
2025-01-13 21:31:39 +0700

    edit app.js

--- backend/app.js
+++ backend/app.js
@@ -1,22 +1,21 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path'); 
-const db = require('./models/index');  
-const authRoutes = require('./routes/authRoutes');  
-const weatherRoutes = require('./routes/weatherRoutes');  
+const db = require('./models/index');
+const authRoutes = require('./routes/authRoutes');
+const weatherRoutes = require('./routes/weatherRoutes');
+const path = require('path');  
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/weather', weatherRoutes);
 
-app.use(express.static(path.join(__dirname, 'frontend')));
-
-app.use('/api/auth', authRoutes);  
-app.use('/api/weather', weatherRoutes);  
+app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'frontend', 'index.html'));
+    res.sendFile(path.join(__dirname, '../frontend', 'index.html')); 
 });
 
 app.listen(PORT, () => {
