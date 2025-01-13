f95e020866b5e1b2f41571f8d5a604847f36f697
kunpriyaa <kunpriyachaiwong21@!gmail.com>
2025-01-13 15:47:41 +0700

    Initial commit

---
+++ backend/app.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const express = require('express');
+const db = require('./models/index');
+const authRoutes = require('./routes/authRoutes');
+const weatherRoutes = require('./routes/weatherRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use('/api/auth', authRoutes);
+app.use('/api/weather', weatherRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
